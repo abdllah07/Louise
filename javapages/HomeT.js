41fa3781bcd13ea6fab18dd337e331d5153511ac
abdallahalhasan09 <ahhmi9p@gmail.com>
2022-08-21 21:03:02 +0300

    Home2

---
+++ javapages/HomeT.js
@@ -0,0 +1,14 @@
+$(function () {
+    $("#mega").click(function () {
+      $(".mega-menu").slideToggle("slow");
+      $(".mega-menu").css("display",'flex');
+    });
+
+
+    $("#mega1").click(function () {
+        $("#mega-menu").slideToggle("slow");
+        $("#mega-menu").css("display",'flex');
+      });
+  });
+  
+
